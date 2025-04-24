af66b64de64584527e0f35f6e4c1409ef0c0cff4
maria <maria.kluge@acad.ufsm.br>
2025-04-23 23:20:21 -0300

    arrumei o trabalho

--- trabalhos/l1-t1-maria.c
+++ trabalhos/l1-t1-maria.c
@@ -1,68 +1,73 @@
-#include<stdio.h>
+#include <stdio.h>
+
 void cor_letra(int r, int g, int b)
+{
+  printf("%c[38;2;%d;%d;%dm", 27, r, g, b);
+}
+
+void cor_normal()
+{
+  printf("%c[0m", 27);
+}
+
+void formata_numero(int num)
+{
+  if (num <= 126)
   {
-    printf("%c[38;2;%d;%d;%dm", 27, r, g, b);
+    cor_letra(0, 255, 255);
+    printf(" %3d ", num);
+    cor_normal();
+    cor_letra(128, 0, 32);
+    printf(" %7b ", num);
+    cor_normal();
+    cor_letra(57, 255, 20);
+    printf(" %x ", num);
+    cor_normal();
+    cor_letra(204, 153, 0);
+    printf(" %c  ", num);
   }
-  void cor_normal()
+  else
   {
-    printf("%c[0m", 27);
+    printf("   ");
   }
-void numero(int num){
-	if(num<=126){
-	cor_letra(0,255,255);
-	printf(" %3d ",num);
-	cor_normal();
-	cor_letra(128,0,32);
-	printf(" %7b ",num);
-	cor_normal();
-	cor_letra(57,255,20);
-    printf(" %x ",num);
-    cor_normal();
-    cor_letra(204,153,0);
-    printf(" %c  ",num);
-
-
-
-}
-else{
-printf("   ");
 }
 
-}
-void linha_tabela(int num){
+void formata_linha(int num)
+{
 
-     for (int i = 0; i < 6; i++) {
-        numero(num +i *16);
+  for (int i = 0; i < 6; i++)
+  {
+    formata_numero(num + i * 16);
+  }
+}
 
+void cabecalho()
+{
+  printf("Tabela ASCII\n");
+  cor_letra(0, 255, 255);
+  printf("DECIMAL | ");
+  cor_normal();
+  cor_letra(128, 0, 32);
+  printf("BINARIO | ");
+  cor_normal();
+  cor_letra(57, 255, 20);
+  printf("HEXADECIMAL | ");
+  cor_normal();
+  cor_letra(204, 153, 0);
+  printf("CARACTER");
+  cor_normal();
+  printf("\n");
 }
-}
-int main(){
-    printf("Tabela ASCII\n");
-    cor_letra(0,255,255);
-    printf("DECIMAL | ");
-    cor_normal();
-    cor_letra(128,0,32);
-    printf("BINARIO | ");
-    cor_normal();
-    cor_letra(57,255,20);
-    printf("HEXADECIMAL | ");
-    cor_normal();
-    cor_letra(204,153,0);
-    printf("CARACTER");
-    cor_normal();
+
+int main()
+{
+  cabecalho();
+  for (int i = 0; i < 16; i++)
+  {
+
+    formata_linha(32 + i);
+
     printf("\n");
-    for(int i=0;i<16;i++){
-
-    linha_tabela(32+i);
-
-
-
-     printf("\n");
-    }
-    return 0;
+  }
+  return 0;
 }
-
-
-
-
-
