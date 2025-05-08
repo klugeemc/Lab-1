c1590848f8f3efda040f6139b1b38e595dcf9d37
Maria Clara Kluge <103641738+klugeemc@users.noreply.github.com>
2025-05-08 10:04:07 -0300

    Create pseudotrabalho_2.c

---
+++ trabalhos/pseudotrabalho_2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void imp_mat(int m[4][4]){
+    for (int i = 0; i<4; i++){
+            printf("\n");
+        for (int j = 0; j<4; j++){
+            printf(" %d |", m[i][j]);
+        }
+    }
+
+}
+void move_cima(int m[4][4])
+{
+
+}
+
+void move_baixo(int m[4][4])
+{
+    for (int i = 0; i<4; i++){
+        for (int j = 0; j<4; j++){
+            if(m[i][j] == 0){
+            m[i][j] = m[i-1][j];
+            m[i-1][j] = 0;
+            }
+        }
+    }
+}
+
+void move_esquerda(int m[4][4])
+{
+
+}
+
+void move_direita(int m[4][4])
+{
+
+}
+
+int main()
+{
+    int m[4][4] = {
+    { 7, 1, 2, 4 },
+    { 5, 12, 14, 6 },
+    { 3, 8, 10, 9 },
+    { 0, 11, 15, 13 },
+    };
+
+    imp_mat(m);
+    move_baixo(m);
+    printf("\n");
+    imp_mat(m);
+}
